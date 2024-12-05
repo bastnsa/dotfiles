426b708cb5aa2eaff8af243f1e7465644f348424
Bastian <122328212+bastnsa@users.noreply.github.com>
2024-12-05 14:40:54 -0600

    Improve zsh prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,18 +1,34 @@
 setopt PROMPT_SUBST
 
-git_status() {
-    if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-	BRANCH=$(git rev-parse --abbrev-ref HEAD)
-        if [[ -n $(git diff --cached) ]] && [[ -n $(git diff) ]]; then
-            echo "%F{red}$BRANCH %F{cyan}λ%f "
-        elif [[ -n $(git diff --cached) ]]; then
-            echo "%F{red}$BRANCH %F{green}λ%f "
-        elif [[ -n $(git diff) ]]; then
-            echo "%F{red}$BRANCH %F{yellow}λ%f "
-        else
-          echo "%F{red}$BRANCH %F{red}λ%f "
-        fi
+is_git_repo() {
+    git rev-parse --is-inside-work-tree >/dev/null 2>&1
+}
+
+git_branch() {
+    # First commit hasn't been made yet
+    if ! git rev-parse HEAD >/dev/null 2>&1; then
+        echo "init"
+    else
+        git rev-parse --abbrev-ref HEAD
     fi
 }
 
-PROMPT='%F{black}ba%f %F{blue}%~%f $(git_status)%F{white}>%f '
+git_status() {
+    if [[ -n $(git diff --cached) ]]; then
+        echo "green"     # Staged changes
+    elif [[ -n $(git diff) ]]; then
+        echo "yellow"    # Changes not staged
+    elif [[ -n $(git ls-files --others --exclude-standard) ]]; then
+        echo "red"       # Untracked files
+    else
+        echo "cyan"      # Clean working directory
+    fi
+}
+
+git_info() {
+    if is_git_repo; then
+        echo "%F{cyan}git:(%F{red}$(git_branch)%F{cyan}) %F{$(git_status)}λ%f "
+    fi
+}
+
+PROMPT='%F{black}ba%f %F{blue}%~%f $(git_info)%F{white}>%f '
