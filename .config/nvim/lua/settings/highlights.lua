fd1e2be9c7813f3845f1e04a84baf0f5aec0240f
Bastian <122328212+bastnsa@users.noreply.github.com>
2024-12-06 18:47:53 -0600

    Update highlight settings

--- .config/nvim/lua/settings/highlights.lua
+++ .config/nvim/lua/settings/highlights.lua
@@ -25,15 +25,17 @@ local c = {
 -- General
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
 vim.api.nvim_set_hl(0, "CursorLineNr", { fg = c.purple, bold = false })
-vim.api.nvim_set_hl(0, "DashboardArt", { fg = c.nord4 })
-vim.api.nvim_set_hl(0, "DashboardIcons", { fg = c.grey })
-vim.api.nvim_set_hl(0, "DashboardKeys", { fg = c.purple })
-vim.api.nvim_set_hl(0, "DashboardPath", { fg = c.nord9, italic = true })
 vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = c.nord1 })
 vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = c.nord1 })
 vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = c.nord1 })
 
--- Comments
+-- Dashboard
+vim.api.nvim_set_hl(0, "DashboardArt", { fg = c.nord4 })
+vim.api.nvim_set_hl(0, "DashboardIcons", { fg = c.grey })
+vim.api.nvim_set_hl(0, "DashboardKeys", { fg = c.purple })
+vim.api.nvim_set_hl(0, "DashboardPath", { fg = c.nord9, italic = true })
+
+-- Documentation
 vim.api.nvim_set_hl(0, "@comment", { fg = c.nord3 })
 vim.api.nvim_set_hl(0, "@string.documentation", { fg = c.green })
 
@@ -80,6 +82,7 @@ vim.api.nvim_set_hl(0, "@string", { fg = c.nord7 })
 vim.api.nvim_set_hl(0, "@operator", { fg = c.grey2 })
 vim.api.nvim_set_hl(0, "@punctuation.delimiter", { fg = c.grey2 })
 vim.api.nvim_set_hl(0, "@punctuation.bracket", { fg = c.nord4 })
+vim.api.nvim_set_hl(0, "@punctuation.special", { fg = c.nord4 })
 
 -- Tags
 vim.api.nvim_set_hl(0, "@tag", { fg = c.nord9 })
