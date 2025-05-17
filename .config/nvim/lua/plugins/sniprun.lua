e92323234089965f5413ad930eff6a211755af52
Bastian <122328212+bastnsa@users.noreply.github.com>
2025-05-16 19:41:32 -0500

    Disable sniprun

--- .config/nvim/lua/plugins/sniprun.lua
+++ .config/nvim/lua/plugins/sniprun.lua
@@ -1,16 +1,4 @@
 return {
 	"michaelb/sniprun",
-	branch = "master",
-	build = "sh install.sh",
-	config = function()
-		require("sniprun").setup({
-			selected_interpreters = { "JS_TS_deno" },
-			display = {
-				"NvimNotify",
-			},
-			display_options = {
-				notification_timeout = 6000,
-			},
-		})
-	end,
+	enabled = false,
 }
