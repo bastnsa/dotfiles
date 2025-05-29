52aae6be81b68b218509ee6b20d5da8e008a1e54
Bastian <122328212+bastnsa@users.noreply.github.com>
2025-05-29 10:24:54 -0500

    Add autocmd to replace alpha

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -1,23 +1,34 @@
--- For conciseness
 local autocmd = vim.api.nvim_create_autocmd
 local core = vim.api.nvim_create_augroup("Core", { clear = true })
 
--- Terminal
-autocmd("TermOpen", {
-	desc = "Set local options for terminal",
+autocmd("VimEnter", {
+	desc = "Set single-key shortcuts on startup screen and empty buffers",
 	group = core,
 	callback = function()
-		vim.cmd("setlocal nonumber norelativenumber")
+		if vim.fn.argc() == 0 and vim.bo.buftype == "" and vim.fn.line("$") == 1 and vim.fn.getline(1) == "" then
+			vim.keymap.set("n", "e", ":Neotree filesystem reveal left toggle<CR>", { buffer = true, silent = true })
+			vim.keymap.set("n", "l", ":Lazy<CR>", { buffer = true, silent = true })
+			vim.keymap.set("n", "f", ":Telescope find_files<CR>", { buffer = true, silent = true })
+			vim.keymap.set("n", "r", ":Telescope oldfiles<CR>", { buffer = true, silent = true })
+			vim.keymap.set("n", "q", ":qa<CR>", { buffer = true, silent = true })
+		end
 	end,
 })
 
--- Text
 autocmd("BufReadPre", {
 	desc = "Enable text wrapping for markdown and text files",
 	pattern = "*.md,*.txt",
 	group = core,
 	callback = function()
 		vim.wo.wrap = true
+	end,
+})
+
+autocmd("TermOpen", {
+	desc = "Set local options for terminal",
+	group = core,
+	callback = function()
+		vim.cmd("setlocal nonumber norelativenumber")
 	end,
 })
 
