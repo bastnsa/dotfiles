37363bc529e0db2fe2f034042ec6dca1466ce691
Bastian <122328212+bastnsa@users.noreply.github.com>
2025-06-02 18:21:05 -0500

    Remove plugin notify

--- .config/nvim/lua/settings/options.lua
+++ .config/nvim/lua/settings/options.lua
@@ -1,40 +1,35 @@
 local opt = vim.opt
 
--- General UI
-opt.relativenumber = true
-opt.number = true
+-- Display
 opt.cursorline = true
-opt.wrap = false
-opt.signcolumn = "yes"
-opt.showmode = false
-opt.showcmd = false
-opt.ruler = false
-opt.fillchars = { eob = " " }
 opt.guicursor = "n-v-c-i:block-Cursor"
+opt.number = true
+opt.relativenumber = true
+opt.ruler = false
+opt.signcolumn = "yes"
+opt.showcmd = false
+opt.showmode = false
 opt.termguicolors = true
-opt.listchars:append({ eol = "¬" })
 
--- General UX
+-- Behavior
+opt.fillchars = { eob = " " }
 opt.scrolloff = 10
 opt.sidescrolloff = 10
-opt.splitright = true
 opt.splitbelow = true
+opt.splitright = true
+opt.wrap = false
 
 -- Indentation
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.expandtab = true
 opt.autoindent = true
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
 
 -- Integrate with system clipboard
 opt.clipboard:append("unnamedplus")
 
 -- Netrw directory (cascading lines)
 vim.cmd("let g:netrw_liststyle = 3")
-
--- Notifications
-local notify = require("notify")
-vim.notify = notify
 
 -- Search
 opt.ignorecase = true
