890ee47b3ab78829ceaf893797a4315a8d9fe0c7
Bastian <122328212+bastnsa@users.noreply.github.com>
2025-06-06 10:50:07 -0500

    improve autocmd for md and txt files

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -1,12 +1,12 @@
 local autocmd = vim.api.nvim_create_autocmd
 local core = vim.api.nvim_create_augroup("Core", { clear = true })
+local map = vim.keymap.set
 
 autocmd("VimEnter", {
 	desc = "Set single-key shortcuts on startup screen and empty buffers",
 	group = core,
 	callback = function()
 		if vim.fn.argc() == 0 and vim.bo.buftype == "" and vim.fn.line("$") == 1 and vim.fn.getline(1) == "" then
-			local map = vim.keymap.set
 			map("n", "e", ":Neotree filesystem reveal_force_cwd float toggle<CR>", { buffer = true, silent = true })
 			map("n", "l", ":Lazy<CR>", { buffer = true, silent = true })
 			map("n", "f", ":FindFiles<CR>", { buffer = true, silent = true })
@@ -18,11 +18,24 @@ autocmd("VimEnter", {
 })
 
 autocmd("BufReadPre", {
-	desc = "Enable text wrapping for markdown and text files",
+	desc = "Enable better wrapping and navigation for markdown and text files",
 	pattern = "*.md,*.txt",
 	group = core,
 	callback = function()
 		vim.wo.wrap = true
+		vim.wo.linebreak = true
+		vim.wo.breakindent = true
+
+		map("n", "I", "g0i", { buffer = true })
+		map("n", "A", "g$a", { buffer = true })
+		map("n", "j", "gj", { buffer = true })
+		map("n", "k", "gk", { buffer = true })
+		map("n", "0", "g0", { buffer = true })
+		map("n", "$", "g$", { buffer = true })
+		map("v", "j", "gj", { buffer = true })
+		map("v", "k", "gk", { buffer = true })
+		map("v", "0", "g0", { buffer = true })
+		map("v", "$", "g$", { buffer = true })
 	end,
 })
 
